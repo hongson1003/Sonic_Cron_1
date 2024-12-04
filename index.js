f775f7b775758efe7f3f8f3457ac6a3737a77704
hongsonit10 <hongsonit10@gmail.com>
2024-12-05 04:59:50 +0700

    delete B, finish done

--- index.js
+++ index.js
@@ -9,18 +9,8 @@ const pingSpringBoot = async () => {
   }
 };
 
-const pingCronB = async () => {
-  try {
-    await axios.get("http://cronjob-b-url/ping");
-    console.log("Pinged Cron Job B successfully!");
-  } catch (error) {
-    console.error("Failed to ping Cron Job B:", error.message);
-  }
-};
-
 const main = async () => {
-  await pingSpringBoot(); // Đợi ping Spring Boot xong
-  await pingCronB(); // Sau khi ping Spring Boot xong, mới ping Cron Job B
+  await pingSpringBoot();
 };
 
-setInterval(main, 60000); // Gọi mỗi 30 giây
+setInterval(main, 60000);
